728822b703232b3b4dfb8da23c52a5ed94a25a50
pranchi3578 <joelj4j@gmail.com>
2019-10-13 05:32:38 +0530

    job display front end done

--- src/Components/Options/jobsnearme.js
+++ src/Components/Options/jobsnearme.js
@@ -1,8 +1,9 @@
-import { List, message, Avatar, Spin } from 'antd';
+import { List, message, Avatar, Spin, Modal } from 'antd';
 import reqwest from 'reqwest';
 import React,{Component} from "react";
 import { fire, auth, provider,db } from "../Home/config.js"
 import InfiniteScroll from 'react-infinite-scroller';
+import JobDisplay from "./jobdisplay.js"
 
 
 const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
@@ -11,12 +12,14 @@ var dup=[];
 class JobList extends Component {
   state = {
     data: [],
+    jobkey:[],
     loading: false,
     hasMore: true,
   };
 
 
   componentDidMount=()=> {
+    var that=this;
     fire.auth().onAuthStateChanged((user) => {
       if(user){
         var query = db.ref('job').orderByKey();
@@ -29,15 +32,18 @@ class JobList extends Component {
             buery.once("value")
             .then(function(snapshot) {
               const data = [];
+              const jobkey=[];
               snapshot.forEach(function(childSnapshot) {
                var key = childSnapshot.key;
               var childData = childSnapshot.val();
               data.push(childData);
-            
+              jobkey.push(key);
+            that.setState({data:data})
+            that.setState({jobkey:jobkey})
             
             });
-            dup=data;
-            console.log(dup);
+            console.log(that.state.jobkey)
+           console.log(that.state.data)
             
             
           });
@@ -92,6 +98,7 @@ class JobList extends Component {
       this.setState({
         hasMore: false,
         loading: false,
+        visible:false
       });
       return;
     }
@@ -102,6 +109,16 @@ class JobList extends Component {
      //   loading: false,
     //  });
    // });
+  };
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  hideModal = () => {
+    this.setState({
+      visible: false,
+    });
   };
 
   render() {
@@ -115,18 +132,20 @@ class JobList extends Component {
           useWindow={false}
         >
           <List
-            dataSource={this.state.data}
-            renderItem={item => (
-              <List.Item key={item.id}>
+            dataSource={[...this.state.data,...this.state.jobkey]}
+            renderItem={item=> (
+              <List.Item key={item.id} actions={[<a key="list-loadmore-edit" 
+                onClick={this.showModal}>select</a>]}>
                 <List.Item.Meta
                   avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    <Avatar src="https://cdn1.iconfinder.com/data/icons/office-and-employment-vol-2-2/48/311_office_employee_person_user_avatar_search_find_candidate_right-512.png" />
                   }
-                  title={<a href="https://ant.design">{item.title}</a>}
-                  description={item.disc}
+                  title={<a href>{item.Title}</a>}
+                  description={item.description}                
                 />
                 <div>Content</div>
               </List.Item>
+              
             )}
           >
             {this.state.loading && this.state.hasMore && (
@@ -136,7 +155,18 @@ class JobList extends Component {
             )}
           </List>
         </InfiniteScroll>
+        <Modal
+          title="Modal"
+          visible={this.state.visible}
+          onOk={this.hideModal}
+          onCancel={this.hideModal}
+          okText="OK"
+          cancelText="CANCEL"
+        >
+              <JobDisplay/>
+           </Modal>
       </div>
+      
     );
   }
 }
